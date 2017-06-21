9bb237e0247c67bd3fbbe701dcc5dc7226e55c8c
LightningFlash <12lightningflash12@gmail.com>
2017-06-21 18:39:14 -0400

    Update and rename f-c-ver1.java to formula-calculator-ver-1.java

---
+++ formula-calculator-ver-1.java
@@ -0,0 +1,14 @@
+package formulas.calculator.java;
+
+public class FormulasCalculatorJava {
+
+    public static void main(String[] args) {
+        System.out.println("|---------------------------------------|");
+        System.out.println("|            Formula Calculator         |");
+        System.out.println("|                 ver. 1.0              |");
+        System.out.println("|   Programmed by: Zackery P. Fleming   |");
+        System.out.println("|---------------------------------------|");
+        System.out.println("||");
+    }
+    
+}
